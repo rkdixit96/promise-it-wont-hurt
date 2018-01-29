b8caab52c0672d46943e653aed5ce1cf04b5899a
krishna_dixit <rkdixit96@gmail.com>
2018-01-29 22:57:21 +0530

    warmUp- Testing for printing of TIMED OUT

--- warmUp.test.js
+++ warmUp.test.js
@@ -7,4 +7,9 @@ describe('Output validation', () => {
     printTimeout('Hello', 300);
     expect(global.setTimeout.mock.calls[0][1]).toBe(300);
   });
+  test('Printed TIMED OUT!', () => {
+    global.console.log = jest.fn();
+    printTimeout('TIMED OUT!', 300);
+    expect(global.console.log).toHaveBeenCalledWith('TIMED OUT!');
+  });
 });
